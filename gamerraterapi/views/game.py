10d969beb87c96733bf550eed535e5efcef85156
Ryan Beiden <rdbeiden@gmail.com>
2020-12-04 17:12:18 -0600

    wip / view all games, categories

---
+++ gamerraterapi/views/game.py
@@ -0,0 +1,41 @@
+from django.core.exceptions import ValidationError
+from django.urls.exceptions import Resolver404
+from django.views.generic.base import View
+from rest_framework import status
+from django.http import HttpResponseServerError
+from rest_framework.decorators import renderer_classes
+from rest_framework.serializers import Serializer
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from gamerraterapi.models import Game
+
+class GameSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Game
+        url = serializers.HyperlinkedIdentityField(
+            view_name='game',
+            lookup_field='id'
+        )
+        fields = ('id', 'url', 'title', 'description', 'designer', 'year_released', 'est_time_to_play', 'age_rec', 'image_url', 'categories')
+        depth = 1
+
+
+class GameViewSet(ViewSet):
+    def retrieve(self, request, pk=None):
+
+        try:
+            game = Game.objects.get(pk=pk)
+            serializer = GameSerializer(game, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+    def list(self, request):
+        games = Game.objects.all()
+
+        serializer = GameSerializer(
+            games, many=True, context={'request': request}
+        )
+
+        return Response(serializer.data)
