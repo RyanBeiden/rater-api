059ec9c348fe8ab5ca9caa30dd81f18ee7bd16a3
Ryan Beiden <rdbeiden@gmail.com>
2021-02-06 10:03:38 -0600

    add admin url

--- gamerrater/urls.py
+++ gamerrater/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import include
 from django.urls import path
+from django.contrib import admin
 from rest_framework import routers
 from gamerraterapi.views import GameViewSet, CategoryViewSet, ReviewViewSet, RatingViewSet, login_user, register_user
 
@@ -16,6 +17,7 @@ urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    path('admin', admin.site.urls),
 ]
 
 if settings.DEBUG is True:
